df675eba40faf20d833a55199ee75210095047e6
MustafaGuer <mustafaguer@yahoo.de>
2022-08-22 17:15:43 +0200

    chore: interface skills

--- src/app/pages/about-area/about-area.component.ts
+++ src/app/pages/about-area/about-area.component.ts
@@ -1,3 +1,4 @@
+import { Skills } from './../../shared/interfaces/skills';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutAreaComponent implements OnInit {
 
+  public skillStack: Skills[] = [{name: 'JavaScript', url: '../../../assets/logos/javascript.svg'}, {name: 'Angular', url: '../../../assets/logos/angular.svg'}];
+// 'JavaScript', 'Angular', 'HTML5', 'CSS3', 'TypeScript', 'ReactJS', 'PHP', 'SCRUM', 'REST API', 'FIREBASE', 'GIT', 'GITHUB'
   constructor() { }
 
   ngOnInit(): void {
