4726023f58d1973352a8f444ccb6da9bc00e8918
MustafaGuer <mustafaguer@outlook.com>
2022-09-04 06:58:33 +0200

    chore: de en tr all texts

--- src/app/shared/const/headlines.ts
+++ src/app/shared/const/headlines.ts
@@ -1,4 +1,4 @@
-const engHeadline: string = `Hello, I\'m Mustafa Gür!\nI\'m a Frontend Web Developer!`;
+const engHeadline: string = `Hello, I\ am Mustafa Gür!\nI'm a Frontend Web Developer!`;
 const deHeadline: string = `Hallo, Ich bin Mustafa Gür!\nIch bin Frontend Web Entwickler!`;
 const trHeadline: string = `Merhaba, benim adim Mustafa Gür!\n Web Yazılım Geliştiricisiyim!`;
 
