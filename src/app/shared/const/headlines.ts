ca5c0505fcc06a32a4b52bcf4025f9d0860aa02b
MustafaGuer <mustafaguer@outlook.com>
2022-09-11 00:23:51 +0200

    chore: text and scroll

--- src/app/shared/const/headlines.ts
+++ src/app/shared/const/headlines.ts
@@ -1,5 +1,5 @@
-const engHeadline: string = `Hello, I\ am Mustafa Gür!\nI'm a Frontend Web Developer!`;
-const deHeadline: string = `Hallo, Ich bin Mustafa Gür!\nIch bin Frontend Web Entwickler!`;
-const trHeadline: string = `Merhaba, benim adim Mustafa Gür!\n Web Yazılım Geliştiricisiyim!`;
+const engHeadline: string = `Hello, I\ am Mustafa Gür!\nI'm a Fullstack Developer!`;
+const deHeadline: string = `Hallo, Ich bin Mustafa Gür!\nIch bin Fullstack Entwickler!`;
+const trHeadline: string = `Merhaba, benim adim Mustafa Gür!\n Fullstack Yazılım Geliştiricisiyim!`;
 
 export { engHeadline, deHeadline, trHeadline };
